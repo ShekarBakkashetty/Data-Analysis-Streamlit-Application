afc64b9376db5d91e7a291dfd2d68d351da64317
ShekarBakkashetty <92913631+ShekarBakkashetty@users.noreply.github.com>
2023-06-15 16:04:38 +1000

    Update Test5.py

--- Test5.py
+++ Test5.py
@@ -1,33 +1,57 @@
 import streamlit as st
 import pandas as pd
 from pandasai import PandasAI
-import streamlit as st
 from pandasai.llm.openai import OpenAI
-import matplotlib
-matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
+st.title("pandas-ai streamlit interface")
 
-st.title("Data Analysis With PandasAI")
-api_key = st.text_input("PLease provide the api key to start your Analysis")
+st.write("A demo interface for [PandasAI](https://github.com/gventuri/pandas-ai)")
+st.write(
+    "Looking for an example *.csv-file?, check [here](https://gist.github.com/netj/8836201)."
+)
 
+if "openai_key" not in st.session_state:
+    with st.form("API key"):
+        key = st.text_input("OpenAI Key", value="", type="password")
+        if st.form_submit_button("Submit"):
+            st.session_state.openai_key = key
+            st.session_state.prompt_history = []
+            st.session_state.df = None
 
-def res(df,a):
-    if a != '':
-        try:
-            with st.spinner("Please Wait....."):
-                st.write(Pandas_ai.run(df,prompt = a))
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            print("Wait a Minute")
-
-if api_key != '':
-        llm = OpenAI(api_key)
-        Pandas_ai = PandasAI(llm)
-        uploaded_file = st.file_uploader('Upload a file')
+if "openai_key" in st.session_state:
+    if st.session_state.df is None:
+        uploaded_file = st.file_uploader(
+            "Choose a CSV file. This should be in long format (one datapoint per row).",
+            type="csv",
+        )
         if uploaded_file is not None:
             df = pd.read_csv(uploaded_file)
-            st.info("File Uploaded Successfully")
-            st.write(df.head())
-            a = st.text_input("How can I help you")
-            st.button("Submit", on_click=res(df, a))
-            #st.download_button("Download CSV", df.to_csv(), mime='text/csv')
+            st.session_state.df = df
+
+    with st.form("Question"):
+        question = st.text_input("Question", value="", type="default")
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            with st.spinner():
+                llm = OpenAI(api_token=st.session_state.openai_key)
+                pandas_ai = PandasAI(llm)
+                x = pandas_ai.run(st.session_state.df, prompt=question)
+
+                fig = plt.gcf()
+                if fig.get_axes():
+                    st.pyplot(fig)
+                st.write(x)
+                st.session_state.prompt_history.append(question)
+
+    if st.session_state.df is not None:
+        st.subheader("Current dataframe:")
+        st.write(st.session_state.df)
+
+    st.subheader("Prompt history:")
+    st.write(st.session_state.prompt_history)
+
+
+if st.button("Clear"):
+    st.session_state.prompt_history = []
+    st.session_state.df = None
