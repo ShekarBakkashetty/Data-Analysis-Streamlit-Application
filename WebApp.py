58818f755d80261fce3202bead7b2357ceecdf5f
ShekarBakkashetty <92913631+ShekarBakkashetty@users.noreply.github.com>
2023-07-25 16:46:58 +1000

    Update WebApp.py

--- WebApp.py
+++ WebApp.py
@@ -1,29 +1,82 @@
-import streamlit as st
+import pyodbc
+import re
 import pandas as pd
-from pandasai import PandasAI
+import openai
 import streamlit as st
-from pandasai.llm.openai import OpenAI
-import matplotlib
-matplotlib.use('TkAgg')
 
-llm = OpenAI('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')
-Pandas_ai = PandasAI(llm)
-st.title("Data Analysis With PandasAI")
-uploaded_file = st.file_uploader('Upload a file')
 
-def res(df,a):
-    if a != '':
-        try:
-            with st.spinner("Please Wait....."):
-                st.write(Pandas_ai.run(df,prompt = a))
-        except Exception as e:
-            print(f"An error occurred: {str(e)}")
-            print("Wait a Minute")
-if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file)
-    st.info("File Uploaded Successfully")
-    st.write(df.head())
-    a = st.text_input("How can I help you")
-    st.button("Submit", on_click=res(df, a))
-    #st.download_button("Download CSV", df.to_csv(), mime='text/csv')
+st.title("Data Analysis Using LLM V1")
+
+conn = None  # Initialize conn to None
+code = None
+
+def connect_to_database(server, database, username, password):
+    driver = '{ODBC Driver 17 for SQL Server}'
+    connection_string = f"DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}"
+
+    try:
+        conn = pyodbc.connect(connection_string)
+        return conn
+    except pyodbc.Error as e:
+        st.error(f"Error connecting to the database: {str(e)}")
+        return None
+
+def my_func(prompt):
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt}])
+    a = completion.choices[0].message.content
+    b = a.replace("\n", " ")
+    return b
+
+
+def code_extract(b):
+    string = b
+    code_pattern = r"```sql(.*?)```"
+    matches = re.findall(code_pattern, string, re.DOTALL)
+    if string.lower().startswith(("select", "SELECT")):
+        code = string
+    else:
+        if matches:
+            code = matches[0].strip()
+        else:
+            code_pattern = r"```(.*?)```"
+            matches = re.findall(code_pattern, string, re.DOTALL)
+            code = matches[0].strip()
+
+    return code
+
+
+server = st.text_input("Please enter the server name")
+database = st.text_input("Please enter the database name")
+username = st.text_input("please enter the user name")
+password = st.text_input("Please enter the password", type="password")
+connect_button = st.button("Connect")
+if connect_button:
+    conn = connect_to_database(server, database, username, password)
+    if conn is not None:
+        #st.write(conn)
+        st.success("Successfully connected to the database!")
+openai.api_key = st.text_input("Please enter the key")
+pre_prompt = st.text_input("Please enter the pre_prompt")
+prompt = st.text_input("How can I help you")
+if st.button("Check the code"):
+    if prompt:
+        prompt = prompt + pre_prompt
+        with st.spinner("Please Wait....."):
+            try:
+                query = my_func(prompt)
+                code = code_extract(query)
+                st.write(code)
+                if conn is None:
+                    conn = connect_to_database(server, database, username, password)
+                df = pd.read_sql_query(code, conn)
+                st.write(df)
+            except (pd.errors.DatabaseError, pyodbc.DatabaseError) as e:
+                # Handle the database-related error
+                st.error(f" There is an error in AI generated Query, "
+                         f" Please re-run once if issue still persists then check the code and redisgn the prompt accordingly and Run: {str(e)}")
+
+            except Exception as e:
+                # Handle other unexpected exceptions
+                st.error(f" opean AI running busy ,Apoligies Please try Again  {str(e)}")
+
 
